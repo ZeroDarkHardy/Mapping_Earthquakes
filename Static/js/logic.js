d5f92288df7ec0387b12eecea86da4a2dedc3928
Matt Hardy <corporalhardy@gmail.com>
2022-01-20 12:29:06 -0800

    periodic save

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -1,8 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -41,13 +41,3 @@ cityData.forEach(function(city) {
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
 });
-
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    pointToLayer: function(feature, latlng) {
-      console.log(feature);
-      return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
-    }
-
-  }).addTo(map);
