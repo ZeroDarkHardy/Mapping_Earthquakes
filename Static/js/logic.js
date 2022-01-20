70b83e1da4e93fdcb841679a33a1a955a144ec96
Matt Hardy <corporalhardy@gmail.com>
2022-01-20 11:29:35 -0800

    periodic save

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -29,8 +29,15 @@ streets.addTo(map);
 // get data from cities.js
 let cityData = cities;
 
-// Loop through the cities array and create one marker for each city
+// Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
     console.log(city)
-    L.marker(city.location).addTo(map);
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "orange",
+        fillColor: '#ffffa1',
+        lineWeight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
 });
